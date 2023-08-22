c60b9d9cfee7ae31ec0bf99dd9dfe03ac3b1b16f
Dan2759 <kwizeradan.kd27@gmail.com>
2023-08-21 17:33:57 -0700

    0-theprintf.c

--- main.h
+++ main.h
@@ -7,10 +7,11 @@
 
 int _printf(const char *format, ...);
 int handle_print(const char *format, int *i, va_list list);
+int handle_conversion(const char *format, int *i, va_list args);
 
 /* The Functions to print chars, numbers and strings */
-int print_char(va_list types);
-int print_string(va_list types);
+int print_char(va_list types, char c);
+int print_string(va_list types, char *str);
 int print_percent(va_list types);
 int print_int(va_list types);
 
